f4a3a3915c4857101b6df5199db7621507500a5c
owayamaani <ashleyamaani@gmail.com>
2022-06-30 09:16:56 +0000

    task commit

---
+++ 0x00-hello_world/-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - prints out "Programming is like building a multilingual puzzle
+*return 0 if well executed and a non-zero otherwise
+*/
+int main(void)
+{
+      puts("\"Programming is like building a multilingual puzzle");
+           return (0);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+*main - prints out "Programming is like building a multilingual puzzle
+*return 0 if well executed and a non-zero otherwise
+*/
+int main(void)
+{      
+      puts("
