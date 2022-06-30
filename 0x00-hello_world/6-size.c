0880aba64f2247c7a152a557d2f61e8c16defb40
owayamaani <ashleyamaani@gmail.com>
2022-06-30 13:10:50 +0000

    task commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints sizes of the various datatypes
- * Return: 0 if well exited and a non-zero if not
+ * main - prints the sizes of the various datatypes
+ * Return: 0 if well exited  and a non zero if not
  */
 int main(void)
 {
@@ -10,6 +10,6 @@ int main(void)
 		printf("Size of an int: %d byte(s)\n", sizeof(int));
 		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-		printf("Size of float: %d byte(s)\n", sizeof(float));
+		printf("Size of a float: %d byte(s)\n", sizeof(float));
 		return (0);
 }
