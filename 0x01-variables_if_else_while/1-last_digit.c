ec2c25b5c5b1dc772263b3c4c39553613ab266fc
owayamaani <ashleyamaani@gmail.com>
2022-07-01 16:06:12 +0000

    task commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,10 +4,10 @@
 
 
 /**
- *  * main - prints the last digit of the random
- *   * number stored in the variable n
- *    * Return: Always 0 (Success)
- *     */
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
