f72f6c68635f59e254c79841a536c969d09c7eed
owayamaani <ashleyamaani@gmail.com>
2022-07-02 01:20:58 +0000

    task commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,13 @@
 /**
  * main - print sinle digit numbers of base 10 from 0,
  * followed by a new line using putchar
- * Return: 0 always
+ * Return: always 0
  */
 int main(void)
 {
 	int a;
 
-	for (a = 18; a < 28; a++)
+	for (a = 47; a < 57; a++)
 	{
 		putchar(a);
 	}
