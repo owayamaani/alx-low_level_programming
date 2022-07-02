dac484fa34a29381d2a514704326e58b4a2d1ae1
owayamaani <ashleyamaani@gmail.com>
2022-07-02 01:35:59 +0000

    task commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main -prints all numbers of base 16 in lowercase
+ * main - prints all numbers of base 16 in lowercase,
  * followed by a new line
- * Return : always 0 
+ * Return: always 0
  */
 int main(void)
 {
