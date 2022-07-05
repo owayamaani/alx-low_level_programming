a312fe913186f9f2ccd41b0a5491fdf7d1329139
owayamaani <ashleyamaani@gmail.com>
2022-07-05 10:00:00 +0000

    task commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets ,in lowercaswe
+ * followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(c);
+		ch++;
+	}
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case ten times
+ * followed by a new line
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
